99141a763ea3c6f9e466f1d67b5dff127fe855da
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2024-03-05 22:32:56 -0600

    cambios

--- src/conn.js
+++ src/conn.js
@@ -1,25 +1,14 @@
 import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    //port: 33069, 
+    host: 'mysql',
+    port: 3306, 
     user: 'paula',
     database: 'f1_db',
     password: 'f1',
-    waitForConnections: true,
-
     connectionLimit: 10,
     queueLimit: 0
 })
 
-pool.getConnection()
-  .then((connection) => {
-    console.log('Connected to the database');
-
-    connection.release();
-  })
-  .catch((err) => {
-    console.error('Error connecting to the database:', err);
-  });
 
 export default pool;
